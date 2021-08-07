dbf8835edf67ec819e58c8d5800c455889ddeee9
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-07 09:47:39 -0700

    ADDED : Multiple Contacts Added

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -9,21 +9,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         AddressBook Book = new AddressBook();
-        Book.addContact();//Calling Add Contacts Method
-        System.out.println(Book.person);
-        System.out.println("Enter the choice \n 1. Edit \n 2. Delete");
-        int op = sc.nextInt();
-        switch (op) {
-            case 1:
-                Book.editContact();
-                System.out.println("You have Entered following data");
-                System.out.println(Book.person);
-                System.out.println("Thank you for Using the Address book");
-                break;
-            case 2:
-                Book.deleteContact();
-                System.out.println("Addess Book details :" + Book.person);
-                break;
-        }
+        Book.getMenu();
+        Book.showContact();
     }
 }
--- src/main/java/com/bridgelabz/ContactDetails.java
+++ src/main/java/com/bridgelabz/ContactDetails.java
@@ -5,37 +5,59 @@ And Declaring The Variables
 And Defining The Getter And Setter Method
  */
 public class ContactDetails {
+    //variable Declaration
     private String firstName;
     private String lastName;
     private String addressCity;
     private String state;
-    private long zip;
-    private long phoneNumber;
     private String email;
-//Getter Setter Method
+    private Long zip;
+    private Long phoneNumber;
+/*
+Declaring The Parametrised Constructor
+To Initialise The Parameters
+ */
+public ContactDetails(String firstName, String lastName, String addressCity, String state,String email, Long zip, Long phoneNumber ) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.addressCity = addressCity;
+    this.state = state;
+    this.email = email;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+}
+public ContactDetails() {
+}
     public String getFirstName() {
-        return firstName;
+    return firstName;
     }
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName){
         this.firstName = firstName;
     }
     public String getLastName() {
-        return lastName;
+    return lastName;
     }
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+    this.lastName = lastName;
     }
     public String getAddressCity() {
-        return addressCity;
+    return addressCity;
     }
     public void setAddressCity(String addressCity) {
-        this.addressCity = addressCity;
+    this.addressCity = addressCity;
     }
     public String getState() {
         return state;
     }
     public void setState(String state) {
         this.state = state;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+    this.email =email;
     }
     public long getZip() {
         return zip;
@@ -46,16 +68,10 @@ public class ContactDetails {
     public long getPhoneNumber() {
         return phoneNumber;
     }
-    public void setPhoneNumber(long phoneNumber) {
+    public void setPhoneNum(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-/*
+    /*
 Declaring The To String Method
 The toString() method returns the string representation of the object
  */
@@ -66,9 +82,9 @@ The toString() method returns the string representation of the object
                 ", lastName='" + lastName + '\'' +
                 ", addressCity='" + addressCity + '\'' +
                 ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
                 ", zip=" + zip +
                 ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
                 '}';
     }
 }
